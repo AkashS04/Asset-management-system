ae75e92a9ed42803c8f61a69ca38d652c270e290
AkashS04 <akashs10111212@gmail.com>
2026-03-27 16:21:50 +0530

    fix: fixed tailwindcss configuration issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider} from "react-redux";
 import { store } from './app/store'
-import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
